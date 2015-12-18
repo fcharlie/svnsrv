9fc42dfa64fca3d7c3a8d6a71aa94a2645ae572f
Force Charlie <ipvb@qq.com>
2015-12-18 10:49:28 +0800

    create version 1.0

--- src/version.h
+++ src/version.h
@@ -7,9 +7,9 @@
 */
 #ifndef SVNSRV_CONFIG_H
 #define SVNSRV_CONFIG_H
-#define SVNSRV_MAJOR 0
+#define SVNSRV_MAJOR 1
 #define SVNSRV_MINOR 0
-#define SVNSRV_SUBOR 1
+#define SVNSRV_SUBOR 0
 #define SVNSRV_FIXOR 0
 
 #define TOSTRING2(x) #x
@@ -17,6 +17,6 @@
 #define TOSTRING(x) TOSTRING1(x)
 
 #define CONFIG_VERSION                                                         \
-  TOSTRING(SVNSRV_MAJOR.SVNSRV_MINOR.SVNSRV_SUBOR.SVNSRV_FIXOR) /// "0.0.1.0"
+  TOSTRING(SVNSRV_MAJOR.SVNSRV_MINOR.SVNSRV_SUBOR.SVNSRV_FIXOR)
 
 #endif

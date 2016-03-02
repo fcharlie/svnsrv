636d8f058e0314f32da1c0b062297d4a03945234
Force Charlie <ipvb@qq.com>
2016-03-02 11:12:53 +0800

    Rewrite SubversionServer

--- src/SubversionServer.cc
+++ src/SubversionServer.cc
@@ -20,64 +20,17 @@
 #include "SubversionSession.hpp"
 #include "klog.h"
 
-class io_service_pool : public boost::noncopyable {
-public:
-  explicit io_service_pool(std::size_t pool_size = (std::max)(
-                               2u, std::thread::hardware_concurrency() * 2))
-      : next_io_service_(0) {
-    if (pool_size == 0) {
-      klogger::Log(klogger::kFatal, "io_service_pool size is 0");
-      throw std::runtime_error("io_service_pool size is 0");
-    }
-    for (std::size_t i = 0; i < pool_size; ++i) {
-      io_service_ptr io_service(new boost::asio::io_service);
-      work_ptr work(new boost::asio::io_service::work(*io_service));
-      io_services_.push_back(io_service);
-      work_.push_back(work);
-    }
-  }
-  void run() {
-    std::vector<std::shared_ptr<boost::thread>> threads;
-    for (auto &it : io_services_) {
-      std::shared_ptr<boost::thread> thread(
-          new boost::thread(boost::bind(&boost::asio::io_service::run, it)));
-      threads.push_back(thread);
-    }
-    for (auto &t : threads) {
-      t->join();
-    }
-  }
-  void stop() {
-    for (auto &i : io_services_) {
-      i->stop();
-    }
-  }
-
-  boost::asio::io_service &get_io_service() {
-    boost::asio::io_service &io_service = *io_services_[next_io_service_];
-    ++next_io_service_;
-    if (next_io_service_ == io_services_.size()) {
-      next_io_service_ = 0;
-    }
-    return io_service;
-  }
-
-private:
-  typedef std::shared_ptr<boost::asio::io_service> io_service_ptr;
-  typedef std::shared_ptr<boost::asio::io_service::work> work_ptr;
-  std::mutex mtx;
-  std::vector<io_service_ptr> io_services_;
-  std::vector<work_ptr> work_;
-  std::size_t next_io_service_;
-};
-
-class SubversionServer {
+class SubversionServer : private boost::noncopyable {
 public:
   SubversionServer(const SubversionServer &) = delete;
   SubversionServer &operator=(const SubversionServer &) = delete;
   explicit SubversionServer(const NetworkServerArgs &networkArgs)
-      : io_service_pool_(networkArgs.poolSize),
-        acceptor_(io_service_pool_.get_io_service()) {
+      : thread_pool_size_(networkArgs.poolSize), acceptor_(io_service_),
+        new_connection_() {
+    if (thread_pool_size_ == 0) {
+      klogger::Log(klogger::kFatal, "io_service_pool size is 0");
+      throw std::runtime_error("io_service_pool size is 0");
+    }
     boost::asio::ip::tcp::endpoint endpoint(
         boost::asio::ip::address::from_string(networkArgs.address),
         networkArgs.port);
@@ -87,28 +40,35 @@ public:
     acceptor_.listen();
     start_accept();
   }
-  ~SubversionServer() {}
-  void run() { io_service_pool_.run(); }
-  void stop() { acceptor_.close(); }
+  void run() {
+    std::vector<std::shared_ptr<boost::thread>> threads;
+    for (std::size_t i = 0; i < thread_pool_size_; ++i) {
+      std::shared_ptr<boost::thread> thread(new boost::thread(
+          boost::bind(&boost::asio::io_service::run, &io_service_)));
+      threads.push_back(thread);
+    }
+    for (std::size_t i = 0; i < threads.size(); ++i)
+      threads[i]->join();
+  }
 
 private:
   void start_accept() {
-    new_session_.reset(
-        new SubversionSession(io_service_pool_.get_io_service()));
-    acceptor_.async_accept(new_session_->socket(),
+    new_connection_.reset(new SubversionSession(io_service_));
+    acceptor_.async_accept(new_connection_->socket(),
                            boost::bind(&SubversionServer::handle_accept, this,
                                        boost::asio::placeholders::error));
   }
   void handle_accept(const boost::system::error_code &e) {
     if (!e) {
-      new_session_->start();
-      klogger::FileFlush();
+      new_connection_->start();
     }
     start_accept();
   }
-  io_service_pool io_service_pool_;
-  SubversionSessionPtr new_session_;
-  tcp::acceptor acceptor_;
+  void handle_stop() { io_service_.stop(); }
+  std::size_t thread_pool_size_;
+  boost::asio::io_service io_service_;
+  boost::asio::ip::tcp::acceptor acceptor_;
+  SubversionSessionPtr new_connection_;
 };
 
 int SubversionServerInitialize(const NetworkServerArgs &networkArgs) {
